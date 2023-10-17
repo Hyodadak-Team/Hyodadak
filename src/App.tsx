124b6aa527a1d2769a66b08e45c355841315f201
0uizi0 <0uizi0@naver.com>
2023-10-17 22:21:45 +0900

    design: 공지사항 메인 페이지 서브영역 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Index from './pages/index'
+import Notice from './pages/notice'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route>
           <Route path="/" element={<Index />} />
+          <Route path="/notice" element={<Notice />} />
         </Route>
       </Routes>
     </div>
---
+++ src/constants/noticeMenu.ts
@@ -0,0 +1,22 @@
+const noticeMenu = [
+  {
+    id: 1,
+    range: 'total',
+    content: '전체 공지 보기',
+    isActive: true,
+  },
+  {
+    id: 2,
+    range: 'ntc',
+    content: '공지만 보기',
+    isActive: false,
+  },
+  {
+    id: 3,
+    range: 'events',
+    content: '이벤트만 보기',
+    isActive: false,
+  },
+]
+
+export default noticeMenu
