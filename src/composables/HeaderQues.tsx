d7139452e6669fd61fce63ded4feb5a6028a6e2e
ssb1565b <ssongsumovie@naver.com>
2023-10-17 21:10:27 +0900

    design: 피드백 반영

--- src/composables/HeaderQues.tsx
+++ src/composables/HeaderQues.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/header.scss'
 
 export default function HeadeQues() {
   const [notice, setNotice] = useState<boolean>(false)
