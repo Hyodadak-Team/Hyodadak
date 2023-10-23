0f7f633a43c98f256bcca1affc5c629e81d335f6
0uizi0 <0uizi0@naver.com>
2023-10-23 15:43:41 +0900

    feat: 이전 및 다음 게시물 DB 정보 가져오기

---
+++ src/utils/setPrevNextValues.ts
@@ -0,0 +1,33 @@
+import { INotice } from '../types/notice'
+
+const setPrevNextValues = (noticeId: number | null, noticeList: INotice[]) => {
+  let prevId
+  let nextId
+  let prevType
+  let prevTitle
+  let nextType
+  let nextTitle
+
+  if (noticeId) {
+    prevId = noticeId - 1
+    nextId = noticeId
+
+    const [prevContent, nextContent] = [
+      noticeList[prevId - 1],
+      noticeList[nextId - 1],
+    ]
+
+    if (prevContent) {
+      prevType = prevContent.type
+      prevTitle = prevContent.title
+    }
+    if (nextContent) {
+      nextType = nextContent.type
+      nextTitle = nextContent.title
+    }
+  }
+
+  return { prevId, nextId, prevType, prevTitle, nextType, nextTitle }
+}
+
+export default setPrevNextValues
