6a0c846a5df94a9f01e1d32d20615b07920bee32
KingJiwon <wldnjs0401@naver.com>
2023-10-23 14:44:31 +0900

    design: 질문상세페이지 생성

---
+++ src/pages/QuestionDetail.tsx
@@ -0,0 +1,22 @@
+import '../styles/QuestionDetail.scss'
+
+export default function DetailPageAnswerer() {
+  return (
+    <div className="innerBox">
+      <div className="questionBoard_main_box_head">
+        <div className="questionBoard_main_box_head_text">
+          <div className="questionBoard_main_box_head_property">
+            <p className="questionBoard_main_box_head_property_point">100</p>
+            <p className="questionBoard_main_box_head_property_category">
+              거북이 · 무인자판기
+            </p>
+            <p className="questionBoard_main_box_head_property_time">2분전</p>
+          </div>
+          <div className="questionBoard_main_box_head_title">
+            <p>서브웨이 어떻게 먹어요...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
