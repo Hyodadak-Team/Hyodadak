eeb7fe71f4932435e0f94759eaebb7f5a6315b0c
dbswp <dbswp980427@gmail.com>
2023-10-27 02:08:59 +0900

    design/simul : 시뮬레이션 설명 파트

--- src/pages/SimulationSubWay/Subway1.tsx
+++ src/pages/SimulationSubWay/Subway1.tsx
@@ -7,11 +7,6 @@ export default function SubWay1() {
       <div className="SubWaySimulation1">
         <div className="SubwayDisplay1">
           <img
-            className="Subway_order"
-            src="/img/키오스크 칸.svg"
-            alt="키오스크 칸"
-          />
-          <img
             className="Subway_home"
             src="/img/서브웨이-홈 1.svg"
             alt="서브웨이 1"
