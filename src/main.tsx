a8bd00d87d89ceaa49826346f72714df749fba6d
0uizi0 <0uizi0@naver.com>
2023-10-28 00:12:56 +0900

    design: antd의 switch 컴포넌트 디자인

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import App from './App'
 import './styles/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#88dd45',
+          colorPrimaryBorder: '#71b93a',
+          colorPrimaryHover: '#71b93a',
+        },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
   </React.StrictMode>,
 )
