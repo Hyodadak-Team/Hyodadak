4c764935f0ee0d079c4062f2984845642dc3050d
rayodesol <sms1995@naver.com>
2023-10-30 18:54:08 +0900

    design: 질문자용 메인페이지 피드백 반영 중

--- src/pages/MainQues.tsx
+++ src/pages/MainQues.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface Notice {
   id: number
+  type: string
   title: string
   date: string
 }
@@ -9,103 +11,119 @@ interface Notice {
 const initialNotices: Notice[] = [
   {
     id: 15,
-    title: '[공지] 효다닥 사용자 무료 교육 안내ㅣ23년 9월 20일(수)',
+    type: '[공지]',
+    title: '효다닥 사용자 무료 교육 안내ㅣ23년 9월 20일(수)',
     date: '23.07.24',
   },
   {
     id: 14,
-    title:
-      '[이벤트] 효다닥 X 오더퀸 결합 서비스 신규 가입 20% 추가 할인 (~12.31)',
+    type: '[이벤트]',
+    title: '효다닥 X 오더퀸 결합 서비스 신규 가입 20% 추가 할인 (~12.31)',
     date: '23.07.22',
   },
-  { id: 13, title: '[공지] 효다닥 서비스 접속 정상화 안내', date: '22.12.10' },
+  {
+    id: 13,
+    type: '[공지]',
+    title: '효다닥 서비스 접속 정상화 안내',
+    date: '22.12.10',
+  },
 ]
 
 export default function MainQues() {
-  const [notices, setNotices] = useState<Notice[]>(initialNotices)
+  // // 더보기 클릭 시, 밑으로 3개씩 공지사항 보이는 기능
+  // const [notices, setNotices] = useState<Notice[]>(initialNotices)
 
-  const loadMoreNotices = () => {
-    const newNotices: Notice[] = [
-      ...notices,
-      {
-        id: 12,
-        title: '[공지] Chrome 91 업데이트에 따른 로그인 오류 안내',
-        date: '22.06.09',
-      },
-      {
-        id: 11,
-        title: '[이벤트] 효다닥 신규 가입자 3개월 무료 제공 혜택',
-        date: '22.05.24',
-      },
-      {
-        id: 10,
-        title: '[공지] 효다닥 서비스 접속 정상화 안내',
-        date: '22.01.10',
-      },
-    ]
-    setNotices(newNotices)
-  }
+  // const loadMoreNotices = () => {
+  //   const newNotices: Notice[] = [
+  //     ...notices,
+  //     {
+  //       id: 12,
+  //       type: '[공지]',
+  //       title: 'Chrome 91 업데이트에 따른 로그인 오류 안내',
+  //       date: '22.06.09',
+  //     },
+  //     {
+  //       id: 11,
+  //       type: '[이벤트]',
+  //       title: '효다닥 신규 가입자 3개월 무료 제공 혜택',
+  //       date: '22.05.24',
+  //     },
+  //     {
+  //       id: 10,
+  //       type: '[공지]',
+  //       title: '효다닥 서비스 접속 정상화 안내',
+  //       date: '22.01.10',
+  //     },
+  //   ]
+  //   setNotices(newNotices)
+  // }
 
   return (
     <>
-      <div className="shortcut_bar">
-        <p>새로운 체험이 업데이트 되었습니다.</p>
-        <div />
-        <p className="highlight">바로가기</p>
-      </div>
+      <Link to="/simulation">
+        <div className="shortcut_bar">
+          <p>새로운 체험이 업데이트 되었습니다.</p>
+          <div />
+          <p className="highlight">바로가기</p>
+        </div>
+      </Link>
 
-      <div className="innerBox">
-        <div className="banner_section">
-          <div className="titles">
-            <p className="main_title">디지털과 친해지도록</p>
-            <p className="main_title">
-              <b>
-                <span>효다닥</span>이 함께합니다
-              </b>
-            </p>
-            <p className="sub_title">
-              <b>우측 바로가기 메뉴</b>로 원하는 서비스를 이용해보세요
-            </p>
-          </div>
-          <div className="btns">
-            <div className="btn_menu">
-              <div className="img_box">
-                <img src="/img/main_ques_question.png" alt="질문하기" />
-              </div>
-              <p>질문하기</p>
+      <div className="banner_section_bg">
+        <div className="innerBox">
+          <div className="banner_section">
+            <div className="titles">
+              <p className="main_title">디지털과 친해지도록</p>
+              <p className="main_title">
+                <b>
+                  <span>효다닥</span>이 함께합니다
+                </b>
+              </p>
+              <p className="sub_title">
+                <b>우측 바로가기 메뉴</b>로 원하는 서비스를 이용해보세요
+              </p>
             </div>
-            <div className="btn_menu">
-              <div className="img_box">
-                <img src="/img/main_ques_answer.png" alt="내 질문보기" />
+            <div className="btns">
+              <div className="btn_menu">
+                <div className="img_box">
+                  <img src="/img/main_ques_question.svg" alt="질문하기" />
+                </div>
+                <p>질문하기</p>
               </div>
-              <p>내 질문보기</p>
-            </div>
-            <div className="btn_menu">
-              <div className="img_box">
-                <img src="/img/main_ques_partner.png" alt="파트너 둘러보기" />
+              <div className="btn_menu">
+                <div className="img_box">
+                  <img src="/img/main_ques_answer.svg" alt="내 질문보기" />
+                </div>
+                <p>내 질문보기</p>
               </div>
-              <p>파트너 둘러보기</p>
+              <div className="btn_menu">
+                <div className="img_box">
+                  <img src="/img/main_ques_partner.svg" alt="파트너 둘러보기" />
+                </div>
+                <p>파트너 둘러보기</p>
+              </div>
             </div>
           </div>
         </div>
+      </div>
 
+      <div className="innerBox">
         <div className="swiper_section">
           {/* swiper로 구현 예정 */}
           <div className="swiper">
             <div className="response_name">
               {/* 이름, 제목 DB로 받아와야 */}
-              <img src="/img/bell_icon.png" alt="알림icon" />
+              <img src="/img/bell_icon.svg" alt="알림icon" />
               <span>차은우</span>님의 답변을 확인해보세요
             </div>
             <div className="reponse_title">Q 서브웨이 어떻게 먹어요...</div>
             <div className="dots">
-              <img src="/img/dot_icon.png" alt="알림icon" />
-              <img src="/img/dot_gray_icon.png" alt="알림icon" />
-              <img src="/img/dot_gray_icon.png" alt="알림icon" />
+              <img src="/img/dot_icon.png" alt="스와이퍼icon" />
+              <img src="/img/dot_gray_icon.png" alt="스와이퍼icon" />
+              <img src="/img/dot_gray_icon.png" alt="스와이퍼icon" />
             </div>
           </div>
           <button type="button">
-            답변 확인하기 <img src="/img/right_icon.png" alt="알림icon" />
+            답변 확인하기 <img src="/img/right-icon.svg" alt="다음icon" />
           </button>
         </div>
 
@@ -113,19 +131,29 @@ export default function MainQues() {
           <p>공지사항</p>
           <div className="notice_list">
             <ul>
-              {notices.map((notice) => (
-                <li key={notice.id} className="notice-item">
-                  <span className="notice-id">{notice.id}</span>
-                  <span className="notice-title">{notice.title}</span>
-                  <span className="notice-date">{notice.date}</span>
+              {initialNotices.map((notice) => (
+                <li key={notice.id}>
+                  <div>{notice.id}</div>
+                  <div className="notice_title">
+                    <div>{notice.type}</div>
+                    <div>{notice.title}</div>
+                  </div>
+                  <div className="notice_date">{notice.date}</div>
                 </li>
               ))}
             </ul>
           </div>
-          <button type="button" onClick={loadMoreNotices}>
-            <img src="/img/plus_icon.png" alt="더보기icon" />
+
+          <Link to="/notice">
+            <button type="button">
+              <img src="/img/plus-icon.svg" alt="더보기icon" />
+              더보기
+            </button>
+          </Link>
+          {/* <button type="button" onClick={loadMoreNotices}>
+            <img src="/img/plus-icon.svg" alt="더보기icon" />
             더보기
-          </button>
+          </button> */}
         </div>
       </div>
     </>
