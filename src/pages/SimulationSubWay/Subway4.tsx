209d04193dc9f09729a08178185d9aba36de9105
dbswp <dbswp980427@gmail.com>
2023-11-03 21:14:28 +0900

    design 시뮬레이션 기본조합  작업 중

--- src/pages/SimulationSubWay/Subway4.tsx
+++ src/pages/SimulationSubWay/Subway4.tsx
@@ -31,6 +31,27 @@ export default function SubWay4() {
                     <span>토스팅</span>
                   </label>
                   <img src="/img/image check.svg" alt="선택 손가락" />
+                  <div className="basic">
+                    <p>기본조합</p>
+                    <div className="basic_option">
+                      <div className="bread">
+                        <p>빵</p>
+                        <p>화이트 토스팅</p>
+                      </div>
+                      <div className="cheeze">
+                        <p>치즈</p>
+                        <p>아메리카 치즈</p>
+                      </div>
+                      <div className="vegi">
+                        <p>야채</p>
+                        <p>양상추 토마토 오이 피망 양파 피클 올리브 할라피뇨</p>
+                      </div>
+                      <div className="souce">
+                        <p>소스/시즈닝</p>
+                        <p>마요네즈 사우스 웨스트 치폴레</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="whole_choose">
