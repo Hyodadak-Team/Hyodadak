a7c217adc1fb7f1c248f99921dd454cee96fccdb
KingJiwon <wldnjs0401@naver.com>
2023-11-06 06:12:37 +0900

    design: 알림 모달 컴포넌트 클래스명 변경

--- src/components_res/AlertModal.tsx
+++ src/components_res/AlertModal.tsx
@@ -2,50 +2,58 @@ import '../styles/AlertModal.scss'
 
 export default function ResAlert() {
   return (
-    <div className="resAlert">
-      <div className="resAlert_box">
-        <p className="resAlert_box_title">거북이님의 파트너 요청이 왔습니다</p>
-        <p className="resAlert_box_detail">
+    <div className="alertModal">
+      <div className="alertModal_box">
+        <p className="alertModal_box_title">
+          거북이님의 파트너 요청이 왔습니다
+        </p>
+        <p className="alertModal_box_detail">
           안녕하세요 답변 주신 내용 잘 확인했습니다. 파트너 가능하실까요?
         </p>
-        <div className="resAlert_box_footer">
-          <p className="resAlert_box_footer_time">3시간 전</p>
-          <div className="resAlert_box_footer_btn">
-            <p className="resAlert_box_footer_btn_refuse">거절</p>
-            <p className="resAlert_box_footer_btn_accept">수락</p>
+        <div className="alertModal_box_footer">
+          <p className="alertModal_box_footer_time">3시간 전</p>
+          <div className="alertModal_box_footer_btn">
+            <p className="alertModal_box_footer_btn_refuse">거절</p>
+            <p className="alertModal_box_footer_btn_accept">수락</p>
           </div>
         </div>
       </div>
-      <div className="resAlert_box">
-        <p className="resAlert_box_title">거북이님의 파트너 요청이 왔습니다</p>
-        <p className="resAlert_box_detail">
+      <div className="alertModal_box">
+        <p className="alertModal_box_title">
+          거북이님의 파트너 요청이 왔습니다
+        </p>
+        <p className="alertModal_box_detail">
           안녕하세요 답변 주신 내용 잘 확인했습니다. 파트너 가능하실까요?
         </p>
-        <div className="resAlert_box_footer">
-          <p className="resAlert_box_footer_time">3시간 전</p>
-          <p className="resAlert_box_footer_btn-done">수락됨</p>
+        <div className="alertModal_box_footer">
+          <p className="alertModal_box_footer_time">3시간 전</p>
+          <p className="alertModal_box_footer_btn-done">수락됨</p>
         </div>
       </div>
-      <div className="resAlert_box">
-        <p className="resAlert_box_title">거북이님의 파트너 요청이 왔습니다</p>
-        <p className="resAlert_box_detail">
+      <div className="alertModal_box">
+        <p className="alertModal_box_title">
+          거북이님의 파트너 요청이 왔습니다
+        </p>
+        <p className="alertModal_box_detail">
           안녕하세요 답변 주신 내용 잘 확인했습니다. 파트너 가능하실까요?
         </p>
-        <div className="resAlert_box_footer">
-          <p className="resAlert_box_footer_time">3시간 전</p>
-          <p className="resAlert_box_footer_btn-done">거절됨</p>
+        <div className="alertModal_box_footer">
+          <p className="alertModal_box_footer_time">3시간 전</p>
+          <p className="alertModal_box_footer_btn-done">거절됨</p>
         </div>
       </div>
-      <div className="resAlert_box">
-        <p className="resAlert_box_title">거북이님의 파트너 요청이 왔습니다</p>
-        <p className="resAlert_box_detail">
+      <div className="alertModal_box">
+        <p className="alertModal_box_title">
+          거북이님의 파트너 요청이 왔습니다
+        </p>
+        <p className="alertModal_box_detail">
           안녕하세요 답변 주신 내용 잘 확인했습니다. 파트너 가능하실까요?
         </p>
-        <div className="resAlert_box_footer">
-          <p className="resAlert_box_footer_time">3시간 전</p>
-          <div className="resAlert_box_footer_btn">
-            <p className="resAlert_box_footer_btn_refuse">거절</p>
-            <p className="resAlert_box_footer_btn_accept">수락</p>
+        <div className="alertModal_box_footer">
+          <p className="alertModal_box_footer_time">3시간 전</p>
+          <div className="alertModal_box_footer_btn">
+            <p className="alertModal_box_footer_btn_refuse">거절</p>
+            <p className="alertModal_box_footer_btn_accept">수락</p>
           </div>
         </div>
       </div>
