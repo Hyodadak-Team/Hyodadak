96831cb6c49a54ed3bb1629cda2d9a4589c27845
0uizi0 <0uizi0@naver.com>
2023-11-13 15:34:58 +0900

    fix: Link 중첩 관련 오류 해결

--- src/components_res/MyResponseBox.tsx
+++ src/components_res/MyResponseBox.tsx
@@ -10,15 +10,17 @@ export default function MyResponseBox({ data }: MyPresponseProps) {
   const { title, type, writer, createTime, money, content } = data
   const difference: string = timeDifference(createTime)
   return (
-    <Link to="detail주소바꾸자" className="question_box">
-      <p className="pink_txt">
-        {money && <span className="money">{money}</span>}
-        {writer} · {type}
-        <span className="time"> {difference}</span>
-      </p>
-      <p className="title">{title}</p>
-      <p className="content">{content}</p>
+    <>
+      <Link to="detail주소바꾸자" className="question_box">
+        <p className="pink_txt">
+          {money && <span className="money">{money}</span>}
+          {writer} · {type}
+          <span className="time"> {difference}</span>
+        </p>
+        <p className="title">{title}</p>
+        <p className="content">{content}</p>
+      </Link>
       <GradientBtn icon="chat" path="chat" content="채팅하기" />
-    </Link>
+    </>
   )
 }
