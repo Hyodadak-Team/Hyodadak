f353b319e6b65e99bf392191d536d606247ec8ff
rayodesol <sms1995@naver.com>
2023-12-11 21:41:53 +0900

    design: 파트너 둘러보기 - 요청 버튼, 요청 보내기 모달, 더보기 버튼

--- src/components_ques/PartnerBox.tsx
+++ src/components_ques/PartnerBox.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PartnerInfo } from '../types/searchPartner'
+import PartnerRequest from './PartnerRequest'
 
 type PartnerBoxProps = {
   data: PartnerInfo
@@ -8,33 +9,60 @@ type PartnerBoxProps = {
 export default function PartnerBox({
   data: { image, name, category, expl, level, answer, point },
 }: PartnerBoxProps) {
+  const [visible, setVisible] = useState(false)
+
+  const showApplyBox = () => {
+    setVisible(!visible)
+  }
+
+  const closeApplyBox = () => {
+    setVisible(!visible)
+  }
+
   return (
-    <div className="PartnerBox_comp">
-      <img src={`../img/${image}`} alt="프로필img" />
-      <div className="info_content">
-        <span className="name">{name}</span> {/* 카테고리 3개까지만 출력 */}
-        <span className="category">
-          {category.map((el, idx) => {
-            let str = ''
-            if (idx < 2 && idx === category.length - 1) {
-              str += el
+    <>
+      <div className="PartnerBox_comp">
+        <img src={`../img/${image}`} alt="프로필img" />
+        <div className="info_content">
+          <span className="name">{name}</span> {/* 카테고리 3개까지만 출력 */}
+          <span className="category">
+            {category.map((el, idx) => {
+              let str = ''
+              if (idx < 2 && idx === category.length - 1) {
+                str += el
+                return str
+              }
+              if (idx < 2) {
+                str += `${el} · `
+              } else if (idx === 2) {
+                str += el
+              } else if (idx === 3) {
+                str += ' ...'
+              }
               return str
+            })}
+          </span>
+          <p className="expl">{expl}</p>
+          <span className="level">Lv.{level}</span>
+          <span className="answer">답변수 {answer}</span>
+          <span className="point">쌓인 용돈 {point}</span>
+        </div>
+        <div
+          className={`btn_apply ${visible ? 'applied' : ''}`}
+          onClick={showApplyBox}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              showApplyBox()
             }
-            if (idx < 2) {
-              str += `${el} · `
-            } else if (idx === 2) {
-              str += el
-            } else if (idx === 3) {
-              str += ' ...'
-            }
-            return str
-          })}
-        </span>
-        <p className="expl">{expl}</p>
-        <span className="level">Lv.{level}</span>
-        <span className="answer">답변수 {answer}</span>
-        <span className="point">쌓인 용돈 {point}</span>
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          파트너 요청하기
+        </div>
       </div>
-    </div>
+      {visible && <div className="blur" />}
+      {visible && <PartnerRequest onClose={closeApplyBox} />}
+    </>
   )
 }
--- src/components_ques/PartnerRequest.tsx
+++ src/components_ques/PartnerRequest.tsx
@@ -1,15 +1,38 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import '../styles/partnerRequest.scss'
 
-export default function PartnerRequest() {
+interface ApplyBoxProps {
+  onClose: () => void
+}
+
+export default function PartnerRequest(props: ApplyBoxProps) {
+  // 모달 닫기
+  const { onClose } = props
+  const closeBox = () => {
+    onClose()
+  }
+
   return (
     <div className="partner_request ques">
       <div className="partner_request_box">
         <div className="partner_request_box_header">
           <p>파트너 요청 보내기</p>
-          <Link to="/">
+          {/* 기존 코드 */}
+          {/* <Link to="/">
             <img src="/img/partner_req_close_icon.svg" alt="닫기" />
-          </Link>
+          </Link> */}
+          {/* 모달 닫기 기능 추가 */}
+          <button
+            type="button"
+            onClick={closeBox}
+            onKeyDown={(e) => {
+              if (e.key === 'Escape') {
+                closeBox()
+              }
+            }}
+          >
+            <img src="/img/partner_req_close_icon.svg" alt="닫기icon" />
+          </button>
         </div>
         <form>
           <textarea
